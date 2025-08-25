b6e897e51531d0651393addc445a6b685e590f8f
Aljon0 <aljons702@gmail.com>
2025-08-25 20:00:27 +0800

    Fix import typo for OrderSummary component in PaymentFlow

--- src/components/PaymentFlow.jsx
+++ src/components/PaymentFlow.jsx
@@ -2,7 +2,7 @@ import { ArrowLeft } from "lucide-react";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ConfirmationOverlay from "./ConfirmationOverlay";
-import OrderSummary from "./OrderSUmmary";
+import OrderSummary from "./OrderSummary";
 import PaymentForm from "./PaymentForm";
 import SuccessMessage from "./SuccessMessage";
 
